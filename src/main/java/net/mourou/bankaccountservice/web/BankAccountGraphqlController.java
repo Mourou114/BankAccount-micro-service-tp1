67eb290cf7bd171ffd8164189b640420cea54f4b
youness mourou <mourou114@gmail.com>
2025-02-19 19:04:21 +0100

    v3 add Graphql

---
+++ src/main/java/net/mourou/bankaccountservice/web/BankAccountGraphqlController.java
@@ -0,0 +1,40 @@
+package net.mourou.bankaccountservice.web;
+
+import net.mourou.bankaccountservice.dto.BankAccountRequestDTO;
+import net.mourou.bankaccountservice.dto.BankAccountResponseDTO;
+import net.mourou.bankaccountservice.entities.BankAccount;
+import net.mourou.bankaccountservice.repositories.BankAccountRepository;
+import net.mourou.bankaccountservice.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+
+
+@Controller
+public class BankAccountGraphqlController {
+    @Autowired
+    private BankAccountRepository bankAccountRepository;
+    @Autowired
+    private AccountService accountService;
+    @QueryMapping
+    public List<BankAccount> accountsList(){
+        return bankAccountRepository.findAll();
+    }
+    @QueryMapping
+    public BankAccount bankAccountById(@Argument String id){
+        return bankAccountRepository.findById(id).orElseThrow(()->new RuntimeException(String.format("Account %s not found",id)));
+    }
+
+    @MutationMapping
+    public BankAccountResponseDTO addAccount(@Argument BankAccountRequestDTO bankAccount){
+        return accountService.addAccount(bankAccount);
+    }
+}
+/*
+record BankAccountDTO (Double balance, String type, String currency){
+}
+*/
